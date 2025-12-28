755ab4d69db2a653e373c4ad9aa5a6025efa055b
Mubi <mubashirwasi7@gmail.com>
2025-12-28 22:57:08 +0500

    feat: onloading router structure created

---
+++ routes/onloading.js
@@ -0,0 +1,9 @@
+import express from 'express';
+
+// Controllers
+const onloadingRouter = express.Router();
+
+// Routes
+// materialRouter.get('/get-material', getMaterial);
+
+export { onloadingRouter };
