8080283f18d9f3a667efcab1fab003b68d84ecc9
muhammadhamza895 <rashiduzzaman65@gmail.com>
2026-01-01 08:00:09 +0500

    feat: Schema created for work order

---
+++ models/workOrder.js
@@ -0,0 +1,33 @@
+import mongoose from "mongoose";
+
+const WorkOrderSchema = new mongoose.Schema(
+    {
+        job: {
+            type: String,
+            required: true,
+        },
+        description: {
+            type: String,
+            trim: true
+        },
+        priority: {
+            type: String,
+            enum: ["low", "medium", "high"],
+            required: true
+        },
+        deliveryDate: {
+            type: Date,
+            required: true
+        }
+    },
+    {
+        timestamps: { createdAt: "createdAt", updatedAt: false }
+    }
+);
+
+WorkOrderSchema.index({ job: 1 });
+WorkOrderSchema.index({ priority: 1 });
+WorkOrderSchema.index({ job: 1, priority: 1,createdAt: -1});
+
+const WorkOrder = mongoose.model("WorkOrder", WorkOrderSchema);
+export default WorkOrder;
