417eb3423b383cc82073aa820b9aee9f938d7183
muhammadhamza895 <rashiduzzaman65@gmail.com>
2026-01-03 11:48:36 +0500

    feat: inventory schema updated

--- models/inventoryTransection.js
+++ models/inventoryTransection.js
@@ -61,9 +61,11 @@ const InventoryTransactionSchema = new mongoose.Schema(
 );
 
 // indexes for performance
+InventoryTransactionSchema.index({ materialId: 1 });
 InventoryTransactionSchema.index({ materialId: 1, createdAt: -1 });
-InventoryTransactionSchema.index({ sourceType: 1, sourceId: 1 });
 InventoryTransactionSchema.index({ sourceId: 1 });
+InventoryTransactionSchema.index({ sourceType: 1, createdAt: -1 });
+InventoryTransactionSchema.index({ sourceType: 1, sourceId: 1 });
 
 const InventoryTransactionModel = mongoose.model(
     "InventoryTransaction",
